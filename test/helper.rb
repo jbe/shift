2868ac0e28aa4456a43903332b4b5a6c65a43bbc
Jostein Berre Eliassen <post@jostein.be>
2011-05-15 11:46:58 +0200

    temp fix for testing on 1.8.7

--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,7 @@ class TestCase < MiniTest::Unit::TestCase
   def self.test(name, &block)
     test_name = "test_#{name.gsub(/\s+/, '_')}".to_sym
     defined = instance_method(test_name) rescue false
-    raise "#{test_name} already defined in #{self}" if defined
+    #raise "#{test_name} already defined in #{self}" if defined
 
     block ||= proc { skip }
     define_method(test_name, &block)
